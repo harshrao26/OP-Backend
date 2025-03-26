5e39851e7c8426c98c35659cc118823d24b9cd52
Harsh D. Rao <harshrao26>
2025-03-26 21:34:00 +0530

    Allow Cors from all origins

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Connect to MongoDB
 connectDB();
 
-app.use(cors({ origin: "https://op-frontend-wdaj.vercel.app", credentials: true })); // Adjust the origin
+app.use(cors({ origin: "*", credentials: true })); // Adjust the origin
 app.use(express.json());
 
 
